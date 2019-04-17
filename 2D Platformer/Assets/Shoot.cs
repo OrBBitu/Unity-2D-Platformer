ab530b17fedfe998e3c2b9e842c78523ce32dc4e
Andrei Nicolaescu <andrei.nicolaescu15@gmail.com>
2019-04-17 13:59:55 +0300

    Enemy death, bullets and impact

---
+++ 2D Platformer/Assets/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        InvokeRepeating("Shot", 2.0f, 1f);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Shot()
+    {
+
+        //shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+
+    }
+}
